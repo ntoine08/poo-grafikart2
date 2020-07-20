80440d8bbec6ad8cc9229d2ed5473559900f69f8
ntoine08 <j.antoine689@laposte.net>
2020-07-20 18:41:50 +0200

    correction video 12

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ ob_start();
 if($p === 'home'){
     require '../pages/home.php';
 }elseif($p === 'single'){
-    require '..pages/single.php';
+    require '../pages/single.php';
 }
 $content = ob_get_clean();
 require '../pages/templates/default.php';
